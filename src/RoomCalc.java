d0944872b2ad7025f24a11d0bc12c3c02af8ac22
thebelltollsforme <thebelltollsforme@gmail.com>
2017-06-04 04:15:04 -0500

    testing commit further

--- src/RoomCalc.java
+++ src/RoomCalc.java
@@ -5,8 +5,13 @@ public class RoomCalc {
    // testing the commit function in netbeans
     // testing again
     // testing if the netbeans commit is more annoying than the cmd prompt
+    // more testing for the commit comment
+    
     public static void main(String[] args) {
         Room testRoom = new Room(1000,1);
+        System.out.println(testRoom);
     }
+    
+    
     
 }
