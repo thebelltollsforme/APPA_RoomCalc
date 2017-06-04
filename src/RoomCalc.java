bdbd9a6b1d98ec85d2bcfa2269532bd1e75f79a0
thebelltollsforme <thebelltollsforme@gmail.com>
2017-06-04 04:26:02 -0500

    Testing tasks while committing Task #1 - Test Task

--- src/RoomCalc.java
+++ src/RoomCalc.java
@@ -6,7 +6,7 @@ public class RoomCalc {
     // testing again
     // testing if the netbeans commit is more annoying than the cmd prompt
     // more testing for the commit comment
-    
+    // modifying for the task update
     public static void main(String[] args) {
         Room testRoom = new Room(1000,1);
         System.out.println(testRoom);
